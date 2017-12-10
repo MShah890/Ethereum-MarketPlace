b9d7f99503e7d11efe067986d0236dc94db5dfca
Mohit Shah <mshah890@gmail.com>
2017-12-10 15:58:36 -0800

    Delete cloudinaryservice.py

--- EthereumMarketPlaceFlask/src/routing/static/cloudinaryservice.py
+++
@@ -1,9 +0,0 @@
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
-cloudinary.config( cloud_name = "dqum1yaun", api_key = "522222318877635", api_secret = "XOneqw3ylLq40iKgzq73zG1HZCI" )
-
-
-def upload(file_path):
-    return cloudinary.uploader.upload(file_path)
